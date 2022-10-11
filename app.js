f2789c5bcd26c467f74109c3b174c583c51f7f09
Abhishek P S <vadakkemadamabhishek@gmail.com>
2022-10-11 15:43:42 +0530

    app.listen added

--- app.js
+++ app.js
@@ -109,5 +109,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500); 
   res.render('error');
 });
-
+ app.listen(3000,()=>{
+  console.log('listening on http://localhost:3000');
+ })
 module.exports = app;
